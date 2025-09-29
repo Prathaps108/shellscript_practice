5d2e8d556ce7015a8f02dae3532d6a8362d7a75e
Thanshv <sravanij.1163@gmail.com>
2025-09-29 15:52:26 +0530

    msg

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+p1=Ramesh
+p2=Suresh
+
+echo "$p1:: Hi $p2, How are you?"
+echo "$p2:: Hello $p1, I am fine. How are you?"
+echo "$p1:: What are you doing?"
+echo "$p2:: Iam learning shellscript$p1"
