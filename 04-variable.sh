14d4a4fe473406b1cb1d6700c709560bafdedb45
Thanshv <sravanij.1163@gmail.com>
2025-09-29 16:13:14 +0530

    msg

--- 04-variable.sh
+++ 04-variable.sh
@@ -5,6 +5,6 @@ P1=$1
 p2=$2
 
 echo "$p1:: welcome to singapore $p2"
-echo "$p2:: Thank you, Finally reached"
+echo "$p2:: Thank you $p1, Finally reached"
 echo "$p1:: So, do we have a deal"
 echo "$p2:: yes we ahve big deal"
