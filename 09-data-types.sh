67b989b9ff3a039dc44b9bbf5c4eb0d71a9f48f4
Thanshv <sravanij.1163@gmail.com>
2025-10-03 16:08:35 +0530

    msg

---
+++ 09-data-types.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#every thing in shell is consider as string
+
+NUMBER1=100
+NUMBER2=200
+
+SUM=$((NUMBER1+NUMBER2))
+echo "sum is :${SUM}"
+
+#one more excution example script for string count
+
+LEADERS=("MODI" "PUTIN" "TRUMP" "LEE")
+echo "all leaders : ${LEADERS[@]}"
+echo "first leader : ${LEADER{0}}"
+
