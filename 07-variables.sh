0a4343c4d773a1f87a683d93bc063c4d88cf21e4
Thanshv <sravanij.1163@gmail.com>
2025-10-03 15:25:47 +0530

    msg

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #date=$(date)
-start_time=$date(date +%s)
+start_time=($date +%s)
 sleep 5
-end_time=$(date +%s)
+end_time=($date +%s)
 total_time=$(($end_time-$start_time))
 echo "script executed in : $total_time seconds"
